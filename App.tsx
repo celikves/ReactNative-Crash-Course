ddb286a635fad2e6c38f18bbc4134e98607977ec
Vesile <vesile_emre@hotmail.com>
2023-05-11 02:10:39 +0300

    Style updates. Icon ionicon added.

--- App.tsx
+++ App.tsx
@@ -1,36 +1,65 @@
 import React from "react";
-import {View,Text,SafeAreaView,StyleSheet} from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import Ant from 'react-native-vector-icons/AntDesign';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+//import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
+        <Text><IonIcon name="github" size={106} color="blue"></IonIcon></Text>
         <Text>Current Weather</Text>
         <Text style={styles.temp}>6</Text>
         <Text style={styles.feels}>Feels like 5</Text>
-        <Text>High:8</Text>
-        <Text>Low:5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High:8 </Text>
+          <Text style={styles.highLow}> Low:5</Text>
+        </View>
       </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.desc}>Its sunny</Text>
+        <Text style={styles.message}>Its perfect t-shirt weather</Text>
+      </View>
+
+
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'pink',
-    flex: 1,
-    alignItems:'center'
-  },
   wrapper: {
-    flex:1
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center'
   },
   temp: {
-    color:'black',
-    fontSize:48
+    color: 'black',
+    fontSize: 48
   },
   feels: {
     fontSize: 30,
-    color:'black'
+    color: 'black'
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  desc: {
+    fontSize:48
+  },
+  message: {
+    fontSize:30
   }
 })
 
