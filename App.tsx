3e0598c01acef8aa4d3bfba2398e2d087709ba20
Vesile <vesile_emre@hotmail.com>
2023-05-18 02:35:45 +0300

    updates for app js folder

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import CurrentWeather from './src/components/screens/currentWeather';
 import UpcomingWeather from './src/components/screens/upcomingWeather';
+import City
+  from './src/components/screens/City';
+  import LoginScreen from './src/components/screens/LoginScreen';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <UpcomingWeather />
+      <LoginScreen />
+
       {/* <OurChild message={ 'How are your today'}/> */}
     </View>
   );
