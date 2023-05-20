74624718845acd3ac0ddf273f855d01fab9b799d
Vesile <vesile_emre@hotmail.com>
2023-05-20 21:17:11 +0300

    React navigation library installed

--- App.tsx
+++ App.tsx
@@ -3,15 +3,13 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import CurrentWeather from './src/components/screens/currentWeather';
 import UpcomingWeather from './src/components/screens/upcomingWeather';
-import City
-  from './src/components/screens/City';
-  import LoginScreen from './src/components/screens/LoginScreen';
+import City from './src/components/screens/City';
+import LoginScreen from './src/components/screens/LoginScreen';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <LoginScreen />
-
       {/* <OurChild message={ 'How are your today'}/> */}
     </View>
   );
