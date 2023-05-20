c7b92242685e3bc22b98f4ea6a169aa552f58115
Vesile <vesile_emre@hotmail.com>
2023-05-20 21:17:37 +0300

    IconText component created

---
+++ src/components/components/IconText.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import {
+    View, Text,StyleSheet
+} from "react-native";
+
+const IconText = (props) => {
+    const { iconName, iconColor,bodyText,bodyTextSytles} = props
+    return (
+        <View></View>
+    )
+}
+
+export defualt IconText;
