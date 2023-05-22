dece9e564f3246bb0c89707d4e620b9c1afdb78d
Vesile <vesile_emre@hotmail.com>
2023-05-22 11:57:29 +0300

    State comments

--- src/components/components/IconText.js
+++ src/components/components/IconText.js
@@ -4,6 +4,12 @@ import {
     View, Text,StyleSheet
 } from "react-native";
 
+//State is an object that contains information about a component that we want to track.
+//In the object we use key-value pairs
+//states helps us to add remove and modify things in out component
+//we can trigger UI updates based on the changes
+//when the state changes the component rerenders.
+
 const IconText = (props) => {
     const { iconName, iconColor,bodyText,bodyTextSytles} = props
     return (
