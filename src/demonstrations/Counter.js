35834d7e63197518d61b02da85436aef2ba5f2c7
Vesile <vesile_emre@hotmail.com>
2023-05-24 01:22:51 +0300

    usestate example usage

---
+++ src/demonstrations/Counter.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+
+import React from "react";
+import { useState, useEffect } from "react";
+
+//useEffect let us perform side effects within our funstion components
+//we can pass in a dependecny array which lets you specify what data , when changed , you would like to trigger a re-render
+// we can additionly use a cleanup function to stop any of our side effects before the component unmounts
+
