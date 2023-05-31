d126e6cb25f7615c12f1b1116ac6b09420f461b7
Vesile <vesile_emre@hotmail.com>
2023-05-31 11:32:18 +0300

    app.js

--- App.tsx
+++ App.tsx
@@ -1,27 +1,21 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
-import CurrentWeather from './src/components/screens/CurrentWeather';
-import UpcomingWeather from './src/components/screens/UpcomingWeather';
-import City from './src/components/screens/City';
-import LoginScreen from './src/components/screens/LoginScreen';
+import {StyleSheet, View} from 'react-native';
+import FirstPage from './bitirme/FirstPage';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-const Tab = createBottomTabNavigator()
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={'Current'} component={CurrentWeather} />
-        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
-        <Tab.Screen name={'City'} component={City} />
-        {/* <OurChild message={ 'How are your today'}/> */}
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <FirstPage />
+    </View>
   );
 };
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
